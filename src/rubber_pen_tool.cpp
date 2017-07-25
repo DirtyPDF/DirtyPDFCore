da411564efb3fd4b2417ee86efc3eb931412dd41
Bruno-sm <bru.1bruno+dirty@gmail.com>
2017-07-25 15:47:55 +0200

    Rewrites the behavior of RubberPenTool to use Page::annotationsByPosition

--- src/rubber_pen_tool.cpp
+++ src/rubber_pen_tool.cpp
@@ -13,20 +13,16 @@ RubberPenTool::RubberPenTool(){
 
 RubberPenTool::RubberPenTool(const RubberPenTool &rubberPenTool){
   setWidth(rubberPenTool.width());
-  m_annotationsSubTypes.insert(Poppler::Annotation::AInk);
+  m_annotationsSubTypes = rubberPenTool.m_annotationsSubTypes;
 }
 
 
 void RubberPenTool::behavior(Page* page, const QMouseEvent &mouseEvent){
-  QList<Poppler::Annotation*> inkAnnotations = page->annotations(m_annotationsSubTypes);
+  QList<Poppler::Annotation*> inkAnnotations = page->annotationsByPosition(m_annotationsSubTypes, mouseEvent.localPos(), width()); //page->annotationByPositon(mouseEvent.localPos(), m_annotationsSubTypes)
   AnnotationManipulator* manipulator;
 
   QList<Poppler::Annotation*>::iterator it;
   for (it = inkAnnotations.begin(); it != inkAnnotations.end(); ++it){
-    manipulator = createAnnotationManipulator((Poppler::InkAnnotation*)*it);
-    manipulator->erasePart(mouseEvent.localPos(), width());
-    delete manipulator;
-    if(((Poppler::InkAnnotation*)*it)->inkPaths().empty())
-      page->removeAnnotation(*it);
+    page->removeAnnotation(*it);
   }
 }
